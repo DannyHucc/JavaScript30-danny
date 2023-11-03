9fdea237b7d7a2f1aa47e24e5ee54260676b5e97
DannyHucc <dannyhu10011101@gmail.com>
2023-11-03 16:44:02 +0800

    feat: add show js thirty by js and icon

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const projects = [
+    {
+        id: 1,
+        title: '01. JS Drum Kit.',
+        image: './images/01.png',
+        demo: '01-JavaScript-Drum-Kit/index.html'
+    },
+]
+
+const jsThirty = document.querySelector('#js-thirty')
+
+function generateJsThirty() {
+    jsThirty.innerHTML = ``
+
+    projects.forEach(project => {
+        jsThirty.innerHTML += `
+        <div class="card project-id-${project.id}" style="width: 18rem;">
+            <a href="${project.demo}" rel="nofollow" target="_blank">
+                <img src="${project.image}" class="card-img-top" alt="JS Drum Kit">
+            
+                <div class="card-body">
+                    <p class="card-text">${project.title}</p>
+                </div>
+            </a>
+        </div >
+        `
+    })
+}
+
+generateJsThirty()
