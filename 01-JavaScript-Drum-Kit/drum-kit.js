829c8e8a3c4bf0c2d328eeb0bc2826e19d7afb0d
DannyHucc <dannyhu10011101@gmail.com>
2023-11-06 15:04:41 +0800

    feat: add playing class list

--- 01-JavaScript-Drum-Kit/drum-kit.js
+++ 01-JavaScript-Drum-Kit/drum-kit.js
@@ -2,9 +2,12 @@
 
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+
     if (!audio) return // if not find audio data-key
     audio.currentTime = 0 // resets the current position to 0
     audio.play()
+    key.classList.add('playing')
 }
 
 window.addEventListener('keydown', playSound)
