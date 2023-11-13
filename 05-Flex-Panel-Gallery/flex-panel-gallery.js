d6cc471c612b2c27e2051f564059c029a89c2bfe
DannyHucc <dannyhu10011101@gmail.com>
2023-11-13 14:12:27 +0800

    feat: add panelController function for open css

--- 05-Flex-Panel-Gallery/flex-panel-gallery.js
+++ 05-Flex-Panel-Gallery/flex-panel-gallery.js
@@ -1,2 +1,15 @@
 'use strict'
 
+function panelController() {
+    const panels = document.querySelectorAll('.panel')
+
+    function clickHandler() {
+        this.classList.toggle('open')
+    }
+
+    panels.forEach(panel => {
+        panel.addEventListener('click', clickHandler)
+    })
+}
+
+panelController()
