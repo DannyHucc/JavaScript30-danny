8aecff8a7e5d27e34e4011a8552b3b6e23977a22
DannyHucc <dannyhu10011101@gmail.com>
2023-11-13 14:29:58 +0800

    feat: add transitionHandler func for open active

--- 05-Flex-Panel-Gallery/flex-panel-gallery.js
+++ 05-Flex-Panel-Gallery/flex-panel-gallery.js
@@ -7,8 +7,15 @@ function panelController() {
         this.classList.toggle('open')
     }
 
+    function transitionHandler(e) {
+        if (e.propertyName.includes('flex-grow')) {
+            this.classList.toggle('open-active')
+        }
+    }
+
     panels.forEach(panel => {
         panel.addEventListener('click', clickHandler)
+        panel.addEventListener('transitionend', transitionHandler)
     })
 }
 
