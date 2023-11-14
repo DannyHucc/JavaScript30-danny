30457364bd194c8a267163aedb2b22155db32fca
DannyHucc <dannyhu10011101@gmail.com>
2023-11-14 16:15:27 +0800

    feat: add get cities function

--- 06-Type-Ahead/type-ahead.js
+++ 06-Type-Ahead/type-ahead.js
@@ -1,1 +1,17 @@
 'use strict'
+
+
+function searchCities() {
+    const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+    const cities = []
+
+    async function getCities() {
+        const blob = await fetch(endpoint)
+        const data = await blob.json()
+        cities.push(...data)
+    }
+
+    getCities()
+}
+
+searchCities()
