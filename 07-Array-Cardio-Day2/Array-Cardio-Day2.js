0047f61cb3e92bc6926283a9789ba9b1c0895621
DannyHucc <dannyhu10011101@gmail.com>
2023-11-15 11:59:56 +0800

    feat: add array cardio day2

--- 07-Array-Cardio-Day2/Array-Cardio-Day2.js
+++ 07-Array-Cardio-Day2/Array-Cardio-Day2.js
@@ -19,13 +19,34 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+const questionOne = people.some(p => {
+    return new Date().getUTCFullYear() - p.year >= 19
+})
+console.log('questionOne:', questionOne)
 
 // Array.prototype.every() // is everyone 19 or older?
+const questionTow = people.every(p => {
+    return new Date().getUTCFullYear() - p.year >= 19
+})
+console.log('questionTow:', questionTow)
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+const questionThree = comments.find(comment => {
+    return comment.id.toString() === '823423'
+})
+console.log('questionThree:', questionThree)
+console.table(questionThree)
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+const findIndex = comments.findIndex(comment => {
+    return comment.id.toString() === '823423'
+})
+const sliceStart = comments.slice(0, findIndex)
+const sliceEnd = comments.slice(findIndex + 1)
+const questionFour = [...sliceStart, ...sliceEnd]
+console.log('questionFour', questionFour)
+console.table(questionFour)
