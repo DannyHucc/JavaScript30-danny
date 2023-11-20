5d26115a65557ee7cecebb55213651ec79dce574
DannyHucc <dannyhu10011101@gmail.com>
2023-11-20 12:04:57 +0800

    feat: add click handler function

--- 10-Hold-Shift-and-Check-Checkboxes/hold-shift-and-check-checkboxes.js
+++ 10-Hold-Shift-and-Check-Checkboxes/hold-shift-and-check-checkboxes.js
@@ -1,2 +1,27 @@
 'use strict'
 
+function inputHandler() {
+    const checkboxes = Array.from(document.querySelectorAll('.inbox input[type="checkbox"]'))
+    let lastCheck = null
+
+    function clickHandler(e) {
+        if (this.checked) {
+            if (e.shiftKey && lastCheck !== null) {
+                let nowCheck = checkboxes.indexOf(this)
+                checkboxes.slice(
+                    Math.min(nowCheck, lastCheck),
+                    Math.max(nowCheck, lastCheck)
+                ).forEach(input => input.checked = true)
+            }
+            lastCheck = checkboxes.indexOf(this)
+        } else {
+            lastCheck = null
+        }
+    }
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('click', clickHandler)
+    })
+}
+
+inputHandler()
