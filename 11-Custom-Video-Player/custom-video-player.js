f17228b1ee5632a0c54f1c1fe815ff432d0b232b
DannyHucc <dannyhu10011101@gmail.com>
2023-11-21 15:10:18 +0800

    feat: add change range function for volume playbackRate

--- 11-Custom-Video-Player/custom-video-player.js
+++ 11-Custom-Video-Player/custom-video-player.js
@@ -5,6 +5,7 @@ function playVideo() {
     const video = player.querySelector('.viewer')
     const toggle = player.querySelector('.toggle')
     const skipButtons = player.querySelectorAll('.skip')
+    const ranges = player.querySelectorAll('.player__slider')
 
     function togglePlay() {
         const method = video.paused ? 'play' : 'pause'
@@ -20,6 +21,11 @@ function playVideo() {
         video.currentTime += parseFloat(this.dataset.skip)
     }
 
+    //volume, playbackRate
+    function changeRange() {
+        video[this.name] = this.value;
+    }
+
     // togglePlay
     video.addEventListener('click', togglePlay)
     toggle.addEventListener('click', togglePlay)
@@ -30,6 +36,10 @@ function playVideo() {
 
     // skip
     skipButtons.forEach(button => button.addEventListener('click', skip))
+
+    // volume, playbackRate
+    ranges.forEach(range => range.addEventListener('change', changeRange))
+    ranges.forEach(range => range.addEventListener('mousemove', changeRange))
 }
 
 playVideo()
