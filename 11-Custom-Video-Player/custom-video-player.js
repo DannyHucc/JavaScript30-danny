c3c228ad4a5cba9ac5418efdd69f34d6a8a07957
DannyHucc <dannyhu10011101@gmail.com>
2023-11-21 14:40:14 +0800

    feat: add change button function

--- 11-Custom-Video-Player/custom-video-player.js
+++ 11-Custom-Video-Player/custom-video-player.js
@@ -10,8 +10,16 @@ function playVideo() {
         video[method]()
     }
 
+    function changeButton() {
+        const icon = this.paused ? '►' : '❚ ❚'
+        console.log(icon)
+        toggle.textContent = icon
+    }
+
     video.addEventListener('click', togglePlay)
     toggle.addEventListener('click', togglePlay)
+    video.addEventListener('play', changeButton)
+    video.addEventListener('pause', changeButton)
 }
 
 playVideo()
