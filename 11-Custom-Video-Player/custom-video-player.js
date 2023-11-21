8cfef09b113c9a7bb947fe1edb4c786544b8d44d
DannyHucc <dannyhu10011101@gmail.com>
2023-11-21 14:54:43 +0800

    feat: add skip function

--- 11-Custom-Video-Player/custom-video-player.js
+++ 11-Custom-Video-Player/custom-video-player.js
@@ -4,6 +4,7 @@ function playVideo() {
     const player = document.querySelector('.player')
     const video = player.querySelector('.viewer')
     const toggle = player.querySelector('.toggle')
+    const skipButtons = player.querySelectorAll('.skip')
 
     function togglePlay() {
         const method = video.paused ? 'play' : 'pause'
@@ -15,10 +16,20 @@ function playVideo() {
         toggle.textContent = icon
     }
 
+    function skip() {
+        video.currentTime += parseFloat(this.dataset.skip)
+    }
+
+    // togglePlay
     video.addEventListener('click', togglePlay)
     toggle.addEventListener('click', togglePlay)
+
+    // changeButton
     video.addEventListener('play', changeButton)
     video.addEventListener('pause', changeButton)
+
+    // skip
+    skipButtons.forEach(button => button.addEventListener('click', skip))
 }
 
 playVideo()
