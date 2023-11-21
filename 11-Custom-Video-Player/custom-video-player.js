6d8a6ff39e06946472d230366f756b493db7f699
DannyHucc <dannyhu10011101@gmail.com>
2023-11-21 14:25:34 +0800

    feat: add toggle play function

--- 11-Custom-Video-Player/custom-video-player.js
+++ 11-Custom-Video-Player/custom-video-player.js
@@ -1,2 +1,17 @@
 'use strict'
 
+function playVideo() {
+    const player = document.querySelector('.player')
+    const video = player.querySelector('.viewer')
+    const toggle = player.querySelector('.toggle')
+
+    function togglePlay() {
+        const method = video.paused ? 'play' : 'pause'
+        video[method]()
+    }
+
+    video.addEventListener('click', togglePlay)
+    toggle.addEventListener('click', togglePlay)
+}
+
+playVideo()
