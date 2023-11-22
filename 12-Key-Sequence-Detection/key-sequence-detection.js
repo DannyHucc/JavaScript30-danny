fdf65a323041d000c1b13fcffd34ac69efb8531e
DannyHucc <dannyhu10011101@gmail.com>
2023-11-22 14:54:54 +0800

    feat: add keyup handler function

--- 12-Key-Sequence-Detection/key-sequence-detection.js
+++ 12-Key-Sequence-Detection/key-sequence-detection.js
@@ -1,2 +1,16 @@
 'use strict'
 
+function keyupHandler() {
+    const secretCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+    const input = []
+    function keySequenceDetection(e) {
+        input.push(e.keyCode)
+        input.splice(0, input.length - secretCode.length)
+        if (input.toString() === secretCode.toString()) {
+            cornify_add()
+        }
+    }
+    window.addEventListener('keyup', keySequenceDetection)
+}
+
+keyupHandler()
