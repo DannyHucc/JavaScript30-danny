239704e893e0349c21c6a976026d5dfff2dae050
DannyHucc <dannyhu10011101@gmail.com>
2023-11-23 10:30:36 +0800

    feat: modify website title

--- 13-Slide-in-on-Scroll/slide-in-on-scroll.js
+++ 13-Slide-in-on-Scroll/slide-in-on-scroll.js
@@ -7,7 +7,7 @@ function debounce(func, wait = 20, immediate = true) {
         var later = function () {
             timeout = null
             if (!immediate) func.apply(context, args)
-        };
+        }
         var callNow = immediate && !timeout
         clearTimeout(timeout)
         timeout = setTimeout(later, wait)
