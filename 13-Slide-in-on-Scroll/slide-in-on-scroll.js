260e023ec6923aee6fb055d5d511e0561a83b9f2
DannyHucc <dannyhu10011101@gmail.com>
2023-11-23 15:03:31 +0800

    feat: modify debounce function

--- 13-Slide-in-on-Scroll/slide-in-on-scroll.js
+++ 13-Slide-in-on-Scroll/slide-in-on-scroll.js
@@ -4,14 +4,16 @@ function debounce(func, wait = 20, immediate = true) {
     var timeout
     return function () {
         var context = this, args = arguments
+
         var later = function () {
             timeout = null
             if (!immediate) func.apply(context, args)
         }
-        var callNow = immediate && !timeout
+
+        if (timeout) return
         clearTimeout(timeout)
         timeout = setTimeout(later, wait)
-        if (callNow) func.apply(context, args)
+        if (immediate) func.apply(context, args)
     }
 }
 
@@ -22,7 +24,7 @@ function scrollWindow() {
         const windowBottom = windowTop + window.innerHeight
 
         images.forEach(image => {
-            const imageMiddle = image.offsetTop + image.height / 2
+            const imageMiddle = image.offsetTop + image.height * 3 / 5
             if (windowTop < imageMiddle && imageMiddle < windowBottom) {
                 image.classList.add('active')
             } else {
