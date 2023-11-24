864335a71ff4c65eeba4fd45b11ae5c7a9f41ad4
DannyHucc <dannyhu10011101@gmail.com>
2023-11-24 12:01:49 +0800

    feat: add 2. Array for ues slice()、splice()

--- 14-JavaScript-References-VS-Copying/javascript-references-vs-copying.js
+++ 14-JavaScript-References-VS-Copying/javascript-references-vs-copying.js
@@ -46,6 +46,18 @@ console.log(players, palyers2)
 // So, how do we fix this? We take a copy instead!
 
 // one way
+console.log('2. Array for ues slice()、splice()')
+console.log(players)
+
+let palyersSlice = players.slice()
+let palyersSplice = players.splice(0, players.length)
+
+palyersSlice[0] = '2. Array for one way'
+palyersSplice[0] = '2. Array for one way'
+
+console.log('players: ', players)
+console.log('palyersSlice: ', palyersSlice)
+console.log('palyersSplice: ', palyersSplice)
 
 // or create a new array and concat the old one in
 
