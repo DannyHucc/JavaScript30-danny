3f25033832f331d64a3c55753c83e9fe44bed7b5
DannyHucc <dannyhu10011101@gmail.com>
2023-11-27 15:25:02 +0800

    feat: add createList function

--- 15-LocalStorage/localStorage.js
+++ 15-LocalStorage/localStorage.js
@@ -4,6 +4,17 @@ const addItems = document.querySelector('.add-items')
 const itemsList = document.querySelector('.plates')
 const items = JSON.parse(localStorage.getItem('items')) || []
 
+function createList(plates = [], platesList) {
+    platesList.innerHTML = plates.map((plate, i) => {
+        return `
+        <li>
+            <input type="checkbox" data-index="${i}" id="item${i}" ${plate.done ? 'checked' : ''}>
+            <label for="item${i}">${plate.text}</label>
+        </li>
+        `
+    }).join('')
+}
+
 function addItem(e) {
     e.preventDefault()
     const input = this.querySelector("[name=item]")
@@ -11,5 +22,9 @@ function addItem(e) {
     items.push({ text: input.value, done: false })
     localStorage.setItem('items', JSON.stringify(items))
     this.reset()
+
+    createList(items, itemsList)
 }
+
 addItems.addEventListener('submit', addItem)
+createList(items, itemsList)
