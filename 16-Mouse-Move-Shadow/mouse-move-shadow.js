6669d756ea24a334aec9c16b2b1ca1b0f091dc0a
DannyHucc <dannyhu10011101@gmail.com>
2023-11-28 12:09:31 +0800

    feat: add mouse move Handler function

--- 16-Mouse-Move-Shadow/mouse-move-shadow.js
+++ 16-Mouse-Move-Shadow/mouse-move-shadow.js
@@ -1,2 +1,32 @@
 'use strict'
 
+function renderTextShadow() {
+    const hero = document.querySelector('.hero')
+
+    function mousemoveHandler(e) {
+        const text = document.querySelector('h1')
+        const length = 300
+        const blurRadius = 1
+        const opacity = 0.6
+        
+        let { offsetX, offsetY } = e
+        if (e.target !== this) {
+            offsetX += e.target.offsetLeft
+            offsetY += e.target.offsetTop
+        }
+
+        const lengthX = Math.floor(offsetX / this.offsetWidth * length) * 2 - length
+        const lengthY = Math.floor(offsetY / this.offsetHeight * length) * 2 - length
+
+        text.style.textShadow = `
+            ${lengthX}px ${lengthY}px ${blurRadius}px rgba(255,0,255,${opacity}),
+            ${lengthX * -1}px ${lengthY}px ${blurRadius}px rgba(0,255,255,${opacity}),
+            ${lengthY}px ${lengthX * -1}px ${blurRadius}px rgba(0,255,0,${opacity}),
+            ${lengthY * -1}px ${lengthX}px ${blurRadius}px rgba(0,0,255,${opacity})
+        `
+    }
+    
+    hero.addEventListener('mousemove', mousemoveHandler)
+}
+
+renderTextShadow()
