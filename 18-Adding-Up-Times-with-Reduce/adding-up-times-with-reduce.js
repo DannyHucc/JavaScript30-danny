a0d69fe50af9d5d2063d4a1cb84b41a8a8b68fca
DannyHucc <dannyhu10011101@gmail.com>
2023-11-30 11:08:10 +0800

    feat: add const seconds for calculate second

--- 18-Adding-Up-Times-with-Reduce/adding-up-times-with-reduce.js
+++ 18-Adding-Up-Times-with-Reduce/adding-up-times-with-reduce.js
@@ -9,6 +9,11 @@ function addingUpTimes() {
     const times = [].map.call(list, item => item.dataset.time)
     const times = [].map.apply(list, [item => item.dataset.time]) 
     */
+
+    const seconds = [...times.map(time => {
+        const [min, sec] = time.split(':') // 5:43 => [5, 43]
+        return min * 60 + sec * 1
+    })]
 }
 
 addingUpTimes()
