7fee6b3f9ee9520de4e89b7de43f1cab4dd773ca
DannyHucc <dannyhu10011101@gmail.com>
2023-11-30 10:53:32 +0800

    feat: add const lists

--- 18-Adding-Up-Times-with-Reduce/adding-up-times-with-reduce.js
+++ 18-Adding-Up-Times-with-Reduce/adding-up-times-with-reduce.js
@@ -2,6 +2,13 @@
 
 function addingUpTimes() {
     const li = document.querySelectorAll('li')
+
+    const lists = [...li].map(item => item.dataset.time)
+    /* same as above
+    const lists = Array.from(li).map(item => item.dataset.time)
+    const lists = [].map.call(li, item => item.dataset.time)
+    const lists = [].map.apply(li, [item => item.dataset.time]) 
+    */
 }
 
 addingUpTimes()
