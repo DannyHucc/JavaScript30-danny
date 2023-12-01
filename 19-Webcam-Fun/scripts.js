b0f247778dbc0979b60a27fc36bd3781d755d51c
DannyHucc <dannyhu10011101@gmail.com>
2023-12-01 11:54:24 +0800

    feat: add getVideo function to play video

--- 19-Webcam-Fun/scripts.js
+++ 19-Webcam-Fun/scripts.js
@@ -3,3 +3,16 @@ const canvas = document.querySelector('.photo')
 const strip = document.querySelector('.strip')
 const snap = document.querySelector('.snap')
 const ctx = canvas.getContext('2d')
+
+async function getVideo() {
+    try {
+        const constraints = { video: true, audio: false }
+        const stream = await navigator.mediaDevices.getUserMedia(constraints)
+        video.srcObject = stream
+        video.play()
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+getVideo()
