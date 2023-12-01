465bbdf17a2d5531480b249acd82514b7bcc0d68
DannyHucc <dannyhu10011101@gmail.com>
2023-12-01 12:52:44 +0800

    feat: modify getVideo function to promise

--- 19-Webcam-Fun/scripts.js
+++ 19-Webcam-Fun/scripts.js
@@ -4,15 +4,16 @@ const strip = document.querySelector('.strip')
 const snap = document.querySelector('.snap')
 const ctx = canvas.getContext('2d')
 
-async function getVideo() {
-    try {
-        const constraints = { video: true, audio: false }
-        const stream = await navigator.mediaDevices.getUserMedia(constraints)
-        video.srcObject = stream
-        video.play()
-    } catch (err) {
-        console.error(err)
-    }
+function getVideo() {
+    const constraints = { video: true, audio: false }
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(localMediaStream => {
+            video.srcObject = localMediaStream
+            video.play()
+        })
+        .catch(err => {
+            console.error(err)
+        })
 }
 
 getVideo()
