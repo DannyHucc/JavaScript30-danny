6684426c1430889cc65791e14200f3f7e13be353
DannyHucc <dannyhu10011101@gmail.com>
2023-12-05 11:01:18 +0800

    feat: add recognition function

--- 20-Speech-Detection/speech-detection.js
+++ 20-Speech-Detection/speech-detection.js
@@ -1,3 +1,27 @@
 'use strict'
 
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
+const recognition = new SpeechRecognition()
+recognition.interimResult = true
+
+let p = document.createElement('p')
+const words = document.querySelector('.words')
+words.appendChild(p)
+
+recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('')
+
+    const happyScript = transcript.replace(/happy/gi, '🤩')
+    p.textContent = happyScript
+    if(e.results[0].isFinal) {
+        p = document.createElement('p')
+        words.appendChild(p)
+    }
+})
+
+recognition.addEventListener('end', recognition.start)
+recognition.start()
