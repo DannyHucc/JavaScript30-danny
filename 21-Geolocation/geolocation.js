2874db5b49756068d42d73ea29dd8f7103e51ded
DannyHucc <dannyhu10011101@gmail.com>
2023-12-06 11:25:07 +0800

    feat: add geolocation function

--- 21-Geolocation/geolocation.js
+++ 21-Geolocation/geolocation.js
@@ -1,2 +1,12 @@
 'use strict'
 
+const arrow = document.querySelector('.arrow')
+const speed = document.querySelector('.speed-value')
+
+// watch over time
+navigator.geolocation.watchPosition((data) => {
+    speed.content = data.coords.speed
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`
+}, (err) => {
+    console.error(err)
+}) 
