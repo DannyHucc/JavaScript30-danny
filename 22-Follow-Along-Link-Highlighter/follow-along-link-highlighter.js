75e85f801acc9a09dbbf06c7fa9295aea34c0200
DannyHucc <dannyhu10011101@gmail.com>
2023-12-07 11:15:57 +0800

    feat: add highlight style

--- 22-Follow-Along-Link-Highlighter/follow-along-link-highlighter.js
+++ 22-Follow-Along-Link-Highlighter/follow-along-link-highlighter.js
@@ -2,6 +2,13 @@
 
 function followAlongLinkHighlighter() {
     const links = document.querySelectorAll('a')
+    const highlight = document.querySelector('.highlight')
+    highlight.style.display = 'none'
+
+    function enterHandler() {
+
+    }
+
     links.forEach(link => {
         link.addEventListener('mouseenter', enterHandler)
     })
