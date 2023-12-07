1984b8d8fd5338031c0203e868555ca6558efcbc
DannyHucc <dannyhu10011101@gmail.com>
2023-12-07 11:35:19 +0800

    feat: add enterHandler function

--- 22-Follow-Along-Link-Highlighter/follow-along-link-highlighter.js
+++ 22-Follow-Along-Link-Highlighter/follow-along-link-highlighter.js
@@ -6,7 +6,19 @@ function followAlongLinkHighlighter() {
     highlight.style.display = 'none'
 
     function enterHandler() {
+        const rects = this.getBoundingClientRect()
+        const position = {
+            width: rects.width,
+            height: rects.height,
+            top: rects.top + window.scrollY,
+            left: rects.left + window.scrollX
+        }
 
+        highlight.style.display = "block"
+        highlight.style.width = `${position.width}px`
+        highlight.style.height = `${position.height}px`
+        highlight.style.top = `${position.top}px`
+        highlight.style.left = `${position.left}px`
     }
 
     links.forEach(link => {
