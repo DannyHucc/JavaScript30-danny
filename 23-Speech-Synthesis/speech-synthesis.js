025768f995add21d674cb8a103f303b43e344786
DannyHucc <dannyhu10011101@gmail.com>
2023-12-08 09:43:15 +0800

    refactor: code space

--- 23-Speech-Synthesis/speech-synthesis.js
+++ 23-Speech-Synthesis/speech-synthesis.js
@@ -4,5 +4,5 @@ const msg = new SpeechSynthesisUtterance()
 let voices = []
 const voicesDropdown = document.querySelector('[name="voice"]')
 const options = document.querySelectorAll('[type="range"], [name="text"]')
-const speakButton = document.querySelector('#speak')
 const stopButton = document.querySelector('#stop')
+const speakButton = document.querySelector('#speak')
