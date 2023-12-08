51fededca3a515d8c0af1340d6ca2422575e66b7
DannyHucc <dannyhu10011101@gmail.com>
2023-12-08 09:56:46 +0800

    feat: add displayVoicesOption function

--- 23-Speech-Synthesis/speech-synthesis.js
+++ 23-Speech-Synthesis/speech-synthesis.js
@@ -6,3 +6,12 @@ const voicesDropdown = document.querySelector('[name="voice"]')
 const options = document.querySelectorAll('[type="range"], [name="text"]')
 const stopButton = document.querySelector('#stop')
 const speakButton = document.querySelector('#speak')
+
+function displayVoicesOption() {
+    voices = this.getVoices()
+    voicesDropdown.innerHTML = voices
+        .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+        .join('')
+}
+
+speechSynthesis.addEventListener('voiceschanged', displayVoicesOption)
