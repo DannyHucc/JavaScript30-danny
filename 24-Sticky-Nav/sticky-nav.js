6768cc7eef26f5ff0530470eb4ffec4eca7c142e
DannyHucc <dannyhu10011101@gmail.com>
2023-12-11 11:54:14 +0800

    feat: add resizeNavOffsetTop function

--- 24-Sticky-Nav/sticky-nav.js
+++ 24-Sticky-Nav/sticky-nav.js
@@ -2,18 +2,26 @@
 
 function stickyNav() {
     const nav = document.querySelector('#main')
+    let navOffsetTop = nav.offsetTop
+
+    function resizeNavOffsetTop() {
+        navOffsetTop = nav.offsetTop
+    }
 
     function fixedMenu() {
-        if (window.scrollY >= nav.offsetTop) {
+        if (window.scrollY >= navOffsetTop) {
             // fix navbar
             document.body.classList.add("fixedMenu")
+            document.body.style.paddingTop = `${nav.offsetHeight}px`
         } else {
             // normal
             document.body.classList.remove("fixedMenu")
+            document.body.style.paddingTop = ""
         }
     }
 
     window.addEventListener('scroll', fixedMenu)
+    window.addEventListener('resize', resizeNavOffsetTop)
 }
 
 stickyNav()
