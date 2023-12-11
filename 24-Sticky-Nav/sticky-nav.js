15821ee39e1f18703bb0dbc7530f3e97109bf566
DannyHucc <dannyhu10011101@gmail.com>
2023-12-11 11:33:07 +0800

    feat: add fixedMenu function

--- 24-Sticky-Nav/sticky-nav.js
+++ 24-Sticky-Nav/sticky-nav.js
@@ -1,1 +1,19 @@
 'use strict'
+
+function stickyNav() {
+    const nav = document.querySelector('#main')
+
+    function fixedMenu() {
+        if (window.scrollY >= nav.offsetTop) {
+            // fix navbar
+            document.body.classList.add("fixedMenu")
+        } else {
+            // normal
+            document.body.classList.remove("fixedMenu")
+        }
+    }
+
+    window.addEventListener('scroll', fixedMenu)
+}
+
+stickyNav()
