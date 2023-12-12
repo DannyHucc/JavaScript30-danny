7023fac34d56e3ecd71cc576e60ff14133f84b2e
DannyHucc <dannyhu10011101@gmail.com>
2023-12-12 11:36:15 +0800

    feat: add linkHandler function

--- 25-Event-Capture-Propagation-Bubbling-and-Once/event-capture-propagation-bubbling-and-once.js
+++ 25-Event-Capture-Propagation-Bubbling-and-Once/event-capture-propagation-bubbling-and-once.js
@@ -3,6 +3,7 @@
 function eventCaptureAndBubbling() {
     const buttons = document.querySelector('button')
     const divs = document.querySelectorAll('div')
+    const links = document.querySelectorAll('a')
 
     function clickHandler() {
         console.log('click button')
@@ -14,9 +15,15 @@ function eventCaptureAndBubbling() {
         console.log(this.className)
     }
 
+    function linkHandler() {
+        console.log('linkHandler')
+    }
+
     buttons.addEventListener('click', clickHandler, { capture: false, once: false })
     divs.forEach(div =>
         div.addEventListener('click', divHandler, { capture: false }))
+    links.forEach(link =>
+        link.addEventListener('click', linkHandler, { capture: false }))
 }
 
 eventCaptureAndBubbling()
