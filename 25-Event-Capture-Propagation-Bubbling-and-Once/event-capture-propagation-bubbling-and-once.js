1953f779122c7c1e6239662472bfb703408c85f0
DannyHucc <dannyhu10011101@gmail.com>
2023-12-12 10:48:06 +0800

    feat: add divHandler function

--- 25-Event-Capture-Propagation-Bubbling-and-Once/event-capture-propagation-bubbling-and-once.js
+++ 25-Event-Capture-Propagation-Bubbling-and-Once/event-capture-propagation-bubbling-and-once.js
@@ -2,12 +2,21 @@
 
 function eventCaptureAndBubbling() {
     const buttons = document.querySelector('button')
+    const divs = document.querySelectorAll('div')
 
     function clickHandler() {
         console.log('click button')
     }
 
+    function divHandler(e) {
+        e.stopPropagation()
+        console.log(e.composedPath())
+        console.log(this.className)
+    }
+
     buttons.addEventListener('click', clickHandler, { capture: false, once: false })
+    divs.forEach(div =>
+        div.addEventListener('click', divHandler, { capture: false }))
 }
 
 eventCaptureAndBubbling()
