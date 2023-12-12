dd20ae453256b0c8955a3215156bcfe21385a662
DannyHucc <dannyhu10011101@gmail.com>
2023-12-12 11:42:04 +0800

    feat: add ulHandler function

--- 25-Event-Capture-Propagation-Bubbling-and-Once/event-capture-propagation-bubbling-and-once.js
+++ 25-Event-Capture-Propagation-Bubbling-and-Once/event-capture-propagation-bubbling-and-once.js
@@ -4,6 +4,7 @@ function eventCaptureAndBubbling() {
     const buttons = document.querySelector('button')
     const divs = document.querySelectorAll('div')
     const links = document.querySelectorAll('a')
+    const ul = document.querySelector('ul')
 
     function clickHandler() {
         console.log('click button')
@@ -16,7 +17,14 @@ function eventCaptureAndBubbling() {
     }
 
     function linkHandler() {
-        console.log('linkHandler')
+        console.log("Binding a click:", this)
+    }
+
+    function ulHandler(e) {
+        const target = e.target
+        if (target.nodeName === 'A') {
+            console.log("Delegate a click:", target)
+        }
     }
 
     buttons.addEventListener('click', clickHandler, { capture: false, once: false })
@@ -24,6 +32,7 @@ function eventCaptureAndBubbling() {
         div.addEventListener('click', divHandler, { capture: false }))
     links.forEach(link =>
         link.addEventListener('click', linkHandler, { capture: false }))
+    ul.addEventListener('click', ulHandler, { capture: false, once: false })
 }
 
 eventCaptureAndBubbling()
