ac0c1c2293ed87fd1754acb6e5cff1f69c5beeb5
DannyHucc <dannyhu10011101@gmail.com>
2023-12-12 10:29:23 +0800

    feat: add clickHandler function

--- 25-Event-Capture-Propagation-Bubbling-and-Once/event-capture-propagation-bubbling-and-once.js
+++ 25-Event-Capture-Propagation-Bubbling-and-Once/event-capture-propagation-bubbling-and-once.js
@@ -1,2 +1,13 @@
 'use strict'
 
+function eventCaptureAndBubbling() {
+    const buttons = document.querySelector('button')
+
+    function clickHandler() {
+        console.log('click button')
+    }
+
+    buttons.addEventListener('click', clickHandler, { capture: false, once: false })
+}
+
+eventCaptureAndBubbling()
