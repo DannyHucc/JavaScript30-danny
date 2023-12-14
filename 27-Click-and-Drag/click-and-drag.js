31a188b81b8ef8b17d384bfb06d24d9b5708cda0
DannyHucc <dannyhu10011101@gmail.com>
2023-12-14 10:19:05 +0800

    feat: add EventListener for clickAndDrag function

--- 27-Click-and-Drag/click-and-drag.js
+++ 27-Click-and-Drag/click-and-drag.js
@@ -1,1 +1,18 @@
 'use strict'
+
+function clickAndDrag() {
+    const items = document.querySelector('.items')
+
+    function starDrag() { }
+
+    function dragHandler() { }
+
+    function stopDrag() { }
+
+    items.addEventListener('mousedown', starDrag)
+    items.addEventListener('mousemove', dragHandler)
+    items.addEventListener('mouseup', stopDrag)
+    items.addEventListener('mouseleave', stopDrag)
+}
+
+clickAndDrag()
