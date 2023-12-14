ac387fe5db8c75aa78cf98af08a775edebe9f6e8
DannyHucc <dannyhu10011101@gmail.com>
2023-12-14 11:09:33 +0800

    feat: add scroll feater

--- 27-Click-and-Drag/click-and-drag.js
+++ 27-Click-and-Drag/click-and-drag.js
@@ -2,12 +2,20 @@
 
 function clickAndDrag() {
     const items = document.querySelector('.items')
+    let startX = 0
 
-    function starDrag() {
+    function starDrag(e) {
         items.classList.add('active')
+        startX = e.pageX
     }
 
-    function dragHandler() { }
+    function dragHandler(e) {
+        if (items.classList.contains('active')) {
+            const endX = e.pageX
+            const move = endX - startX
+            items.scrollLeft -= move
+        }
+    }
 
     function stopDrag() {
         items.classList.remove('active')
