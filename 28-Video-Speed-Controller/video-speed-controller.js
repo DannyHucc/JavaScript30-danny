1bc24b6afb7997754b75f220d246777c154772c0
DannyHucc <dannyhu10011101@gmail.com>
2023-12-15 16:07:04 +0800

    feat: add speedBar textContent feature

--- 28-Video-Speed-Controller/video-speed-controller.js
+++ 28-Video-Speed-Controller/video-speed-controller.js
@@ -10,6 +10,13 @@ function videoSpeedController() {
         const percent = Y / this.offsetHeight // 0~1
         const height = `${Math.round(percent * 100)}%`
         speedBar.style.height = height
+
+        // speedBar textContent
+        const max = 10
+        const min = 0.25
+        let range = Math.round((percent * (max - min) + min) * 100) / 100 // 0.25~10
+        range = range > max ? max : range < min ? min : range
+        speedBar.textContent = `${range}x`
     }
 
     speed.addEventListener('mousemove', moveHandler)
