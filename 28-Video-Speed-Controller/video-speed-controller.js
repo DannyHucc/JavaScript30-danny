61772fd50d6edab0f057f7cecd23e528ec5f15c3
DannyHucc <dannyhu10011101@gmail.com>
2023-12-15 15:52:38 +0800

    feat: add speedBar style feature

--- 28-Video-Speed-Controller/video-speed-controller.js
+++ 28-Video-Speed-Controller/video-speed-controller.js
@@ -1,1 +1,18 @@
 'use strict'
+
+function videoSpeedController() {
+    const speed = document.querySelector('.speed')
+
+    function moveHandler(e) {
+        // speedBar style
+        const speedBar = document.querySelector('.speed-bar')
+        const Y = e.pageY - speedBar.offsetTop
+        const percent = Y / this.offsetHeight // 0~1
+        const height = `${Math.round(percent * 100)}%`
+        speedBar.style.height = height
+    }
+
+    speed.addEventListener('mousemove', moveHandler)
+}
+
+videoSpeedController()
