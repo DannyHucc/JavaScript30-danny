bb947a4a65bc5417351e2c918a46ee79acf3cdc5
DannyHucc <dannyhu10011101@gmail.com>
2023-12-18 10:28:21 +0800

    feat: add submitHandler function

--- 29-Countdown-Timer/countdown-timer.js
+++ 29-Countdown-Timer/countdown-timer.js
@@ -3,12 +3,22 @@
 
 function countdownTimer() {
     const buttons = document.querySelectorAll('.timer__controls > button')
+    const input = document.querySelector('#custom')
 
     function setTimer() {
         const sec = parseInt(this.dataset.time)
     }
 
+    function submitHandler(e) {
+        e.preventDefault()
+        const value = parseInt(this.minutes.value)
+        if (!value) return
+        const min = value * 60
+        this.reset()
+    }
+
     buttons.forEach(button => button.addEventListener('click', setTimer))
+    input.addEventListener('submit', submitHandler)
 }
 
 countdownTimer()
