9cea173ab58444d51929fea84532c1d90aa16ee7
DannyHucc <dannyhu10011101@gmail.com>
2023-12-19 10:47:17 +0800

    feat: add startGame and setScore function

--- 30-Whack-A-Mole/whack-a-mole.js
+++ 30-Whack-A-Mole/whack-a-mole.js
@@ -1,2 +1,21 @@
 'use strict'
 
+function whackAMole() {
+    const scoreBoard = document.querySelector('.score')
+    const button = document.querySelector('.startGame')
+
+    let score = 0
+
+    function setScore(s) {
+        score = s
+        scoreBoard.textContent = score
+    }
+
+    function startGame() {
+        setScore(0)
+    }
+
+    button.addEventListener('click', startGame)
+}
+
+whackAMole()
